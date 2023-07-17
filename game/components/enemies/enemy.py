926df70522805a7d9a117ae887b650106ae41ec6
SergioAmarillo <sergio.amarillo@fundacion-jala.org>
2023-07-16 21:04:47 -0500

    tarea 2  agregar nave 2  y aumento de velocidad de la misma

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,7 +2,7 @@ import random
 import pygame
 from pygame.sprite import Sprite
 
-from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import ENEMY_1, ENEMY_2, SCREEN_HEIGHT, SCREEN_WIDTH
 
 LEFT = "left"
 RIGHT = "right"
@@ -12,19 +12,22 @@ RIGHT = "right"
 class Enemy(Sprite):
     X_POS_LIST = [i for i in range(50, SCREEN_WIDTH, 50)]
     Y_POS = 20
-    SPEED_X = 5
+    SPEED_X = random.randint (3, 7)
     SPEED_Y = 1
     def __init__(self):
-        self.image = pygame.transform.scale(ENEMY_1, (50,50))
+        enemy_type = random.choice([ENEMY_1, ENEMY_2])
+        self.image = pygame.transform.scale(enemy_type, (50, 50))
         self.rect = self.image.get_rect()
         self.rect.x = random.choice(self.X_POS_LIST)
         self.rect.y = self.Y_POS
-        self.speed_x = self.SPEED_X
         self.speed_y = self.SPEED_Y
+        self.speed_x = self.SPEED_X
 
         self.movement = random.choice([LEFT, RIGHT])
         self.move_x = random.randint(30, 100)
         self.moving_index = 0
+        if enemy_type == ENEMY_2:
+            self.speed_x  = random.randint (8,12)
 
     def update(self, enemies):
         self.rect.y += self.speed_y
@@ -34,8 +37,6 @@ class Enemy(Sprite):
            self.rect.x -= self.speed_x
 
         self.update_movement()
-        print(self.movement)
-        print(self.moving_index)
         if self.rect.y >= SCREEN_HEIGHT:
             enemies.remove(self)
     
